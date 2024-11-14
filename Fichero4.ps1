cb799a5eb496b6e6207872bbae5ebb94d2abb2e0
Juan <goheju_aliesma2324@iesmedinaazahara.es>
2024-11-14 14:23:55 +0100

    archivo 4 donete

--- Fichero4.ps1
+++ Fichero4.ps1
@@ -6,6 +6,8 @@ Write-Host "4. Obtener dirección IP del adaptador wifi. "
 Write-Host "5. Ver la tabla de enrutamiento de IPv4. "
 Write-Host "6. Realizar un apagado programado de otra máquina "
 Write-Host "7. Crear una barra de progreso "
+Write-Host "8. Salir "
+$x=Read-Host "Seleccione una acción: "
 if ($x -eq 1 ){
 
     Get-NetTCPConnection -state Listen
@@ -30,8 +32,23 @@ if ($x -eq 6 ){
 
     $PC = Read-Host "Nombre o Ip de la maquina a conectar "
     $credenciales = Get-Credential
+    Invoke-Command -ComputerName $PC -Credential $credenciales -ScriptBlock{
+        Start-Process shutdown.exe -ArgumentList "/s /t 0" -NoNewWindow
+    }
      
 }
+if ($x -eq 7 ){
+
+     $progress = 0
+            $durationInSeconds = 10
+            $interval = $durationInSeconds / 2  
+            while ($progress -lt 100) {
+                $progress++
+                Write-Progress -PercentComplete $progress -Status "Processing" -Activity "$i de 100"
+                Start-Sleep -milliseconds $interval
+            }
+            Write-Host "Proceso completado."
 }
-while($x -ne 8){
+}
+while($x -ne 9){
 }
